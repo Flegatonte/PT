45fc8e86bb6f5aa82fea2fc60c1f751a22759229
Sacripantens <53357050+Sacripantens@users.noreply.github.com>
2021-06-04 12:25:55 +0200

    Library view completed

---
+++ T2/Library.View/EditReaderView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Library.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library.View
+{
+    /// <summary>
+    /// Interaction logic for EditReaderView.xaml
+    /// </summary>
+    public partial class EditReaderView : Window, IWindow
+    {
+        public EditReaderView()
+        {
+            InitializeComponent();
+            ReaderViewModel readerViewModel = (ReaderViewModel)DataContext;
+            readerViewModel.MessageBoxShowDelegate = text => MessageBox.Show(text, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)    //makes window be reusable
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
